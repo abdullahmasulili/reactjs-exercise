b3017f9c1cf56c25313c13ffd21fa929e92a300e
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-05 08:54:14 +0800

    patch(InputNumber): fix styling

--- investment-calculator/src/components/InputNumber.jsx
+++ investment-calculator/src/components/InputNumber.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Input({ id, label }) {
   return (
-    <label htmlFor={id}>
-      <span>{label}</span>
+    <div>
+      <label htmlFor={id}>
+        <span>{label}</span>
+      </label>
       <input type="number" id={id} />
-    </label>
+    </div>
   );
 }

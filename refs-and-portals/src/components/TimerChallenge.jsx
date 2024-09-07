66c5321975131c90d1a2c12f8142da110a9f808d
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-07 17:12:21 +0800

    feat(components): add TimerChallenge component

---
+++ refs-and-portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {`${targetTime} second${targetTime > 1 ? "s" : ""}`}
+      </p>
+    </section>
+  );
+}

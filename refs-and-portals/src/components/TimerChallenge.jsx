573a5b91178512c0938316851c569d4493b1d460
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-08 14:51:26 +0800

    feat(TimerChallenge): add start and stop timer handler

--- refs-and-portals/src/components/TimerChallenge.jsx
+++ refs-and-portals/src/components/TimerChallenge.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const [isStart, setIsStart] = useState(false);
+  const [isTimeout, setIsTimeout] = useState(false);
+
+  const timer = useRef();
+
+  function handleStartTimer() {
+    setTimeout(() => {
+      setIsTimeout(true);
+    }, targetTime * 1000);
+
+    setIsStart(true);
+  }
+
+  function handleStopTimer() {
+    clearTimeout(timer.current);
+    setIsStart(false);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
       <p className="challenge-time">
         {`${targetTime} second${targetTime > 1 ? "s" : ""}`}
+      </p>
+      <p>
+        <button onClick={isStart ? handleStopTimer : handleStartTimer}>
+          {isStart ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={isStart ? "acitve" : undefined}>
+        {isStart ? "Time is running..." : "Timer inactive"}
       </p>
     </section>
   );

078cdf7e0bd601320a057b199e6f5c10120810e1
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-10 12:19:16 +0800

    feat(components): add sidebar

---
+++ project-management/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import Button from "./Button";
+
+export default function Sidebar() {
+  return createPortal(
+    <div className="w-80 bg-stone-950 p-8 pt-20 text-white h-full fixed rounded-tr-3xl flex flex-col gap-8">
+      <h1 className="text-3xl font-bold uppercase">Your Projects</h1>
+      <Button className="rounded-lg bg-stone-700 px-4 py-2 hover:bg-stone-600 transition duration-200">
+        + Add Project
+      </Button>
+      <nav className="list-none flex flex-col gap-1">
+        <li className="p-2 hover:bg-stone-700 transition duration-200">
+          Lorem
+        </li>
+        <li className="p-2 hover:bg-stone-700 transition duration-200">
+          Ipsum
+        </li>
+      </nav>
+    </div>,
+    document.querySelector("#sidebar")
+  );
+}

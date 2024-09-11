f975d6a589eaed228b9f98f0e6f526525fd73047
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-11 09:52:28 +0800

    feat(Project): render project detail data to DOM element

--- project-management/src/components/Project.jsx
+++ project-management/src/components/Project.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Button from "./Button";
 
-export default function Project() {
+export default function Project({ projectData }) {
+  const { title, description, dueDate } = projectData || {};
+
+  function formateDate(dateString) {
+    const [year, month, day] = dateString.split("-");
+    const date = new Date(year, month - 1, day);
+
+    return date.toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+  }
+
   return (
     <section id="project" className="max-w-3xl flex-grow pt-20">
       <div className="flex items-center justify-between mb-2">
-        <h1 className="text-3xl font-bold text-stone-600">Lorem Ipsum</h1>
+        <h1 className="text-3xl font-bold text-stone-600">{title}</h1>
         <Button customClass="hover:bg-stone-700 hover:text-stone-100">
           Delete
         </Button>
       </div>
-      <span className="text-stone-500">Sep 09, 2024</span>
-      <p className="text-stone-600 mt-4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos,
-        possimus. Commodi incidunt perspiciatis animi. Fugiat, quasi? Beatae
-        accusantium eaque corrupti, eligendi aut sequi consectetur esse
-        explicabo, neque, inventore libero. Omnis!
-      </p>
+      <span className="text-stone-500">{formateDate(dueDate)}</span>
+      <p className="text-stone-600 mt-4">{description}</p>
     </section>
   );
 }

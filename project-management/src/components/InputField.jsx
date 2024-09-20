d590dcc8faed90ff67c4e437e626be1a9ae58804
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-20 16:48:37 +0800

    refactor(InputField): clean up code

--- project-management/src/components/InputField.jsx
+++ project-management/src/components/InputField.jsx
@@ -4,19 +4,25 @@ const InputField = forwardRef(function InputField(
   { label, isTextarea, ...props },
   ref
 ) {
-  let input = !isTextarea ? (
-    <input
-      {...props}
-      ref={ref}
-      className="h-8 rounded-sm bg-stone-200 focus:outline-none px-2 border-b-2 border-b-stone-400 focus:border-b-stone-600 transition"
-    />
-  ) : (
-    <textarea
-      ref={ref}
-      {...props}
-      className="rounded-sm bg-stone-200 focus:outline-none p-2 border-b-2 border-b-stone-400 focus:border-b-stone-600 transition"
-    ></textarea>
-  );
+  let input;
+
+  if (isTextarea) {
+    input = (
+      <textarea
+        ref={ref}
+        {...props}
+        className="rounded-sm bg-stone-200 focus:outline-none p-2 border-b-2 border-b-stone-400 focus:border-b-stone-600 transition"
+      ></textarea>
+    );
+  } else {
+    input = (
+      <input
+        {...props}
+        ref={ref}
+        className="h-8 rounded-sm bg-stone-200 focus:outline-none px-2 border-b-2 border-b-stone-400 focus:border-b-stone-600 transition"
+      />
+    );
+  }
 
   return (
     <div className="flex flex-col gap-2">

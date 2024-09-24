8709077f8e4a55af6a0b3e7f9429132c90197293
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-24 16:14:34 +0800

    feat(DeleteConfirmation): add auto confirmation

--- handling-side-effect/src/components/DeleteConfirmation.jsx
+++ handling-side-effect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const autoConfirm = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(autoConfirm);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>

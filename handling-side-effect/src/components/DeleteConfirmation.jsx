93942b6d34dbef17f0fd05c9dcfcfc37a7581099
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-24 16:51:37 +0800

    feat(DeleteConfirmation): add progress element to show remaining confirmation time

--- handling-side-effect/src/components/DeleteConfirmation.jsx
+++ handling-side-effect/src/components/DeleteConfirmation.jsx
@@ -1,10 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const progress = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(progress);
+    };
+  }, []);
+
   useEffect(() => {
     const autoConfirm = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       clearTimeout(autoConfirm);
@@ -23,6 +37,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }

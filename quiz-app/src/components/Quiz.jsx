18d5f18fc27bda54dfc5be197801b37a4f6a1e94
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-24 17:19:24 +0800

    feat(App): create base layout for the app

---
+++ quiz-app/src/components/Quiz.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Quiz() {
+  return (
+    <section id="quiz">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt fuga
+      eius voluptates adipisci modi amet officiis atque ut placeat nihil. Quas
+      nostrum vero modi similique, id eaque! Odit, esse. Dolores.
+    </section>
+  );
+}

14d76d6d487cff5d284d44b19c169d179004173b
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-09-30 14:56:33 +0800

    refactor(components): transform into class-based components

--- class-based-components/src/components/User.js
+++ class-based-components/src/components/User.js
@@ -1,7 +1,15 @@
-import classes from './User.module.css';
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import React, { Component } from "react";
 
-export default User;
+export default class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+// export default User;

dfc796822c97700782e78ef97e5c216288ba50fe
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-01 14:12:20 +0800

    feat(AvailablePlaces): add error handling

--- sending-http-requests/src/components/AvailablePlaces.jsx
+++ sending-http-requests/src/components/AvailablePlaces.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     fetchPlaces();
@@ -11,13 +13,31 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   async function fetchPlaces() {
     setIsFetching(true);
-    const response = await fetch(import.meta.env.VITE_BASE_API + "/places", {
-      method: "GET",
-    });
-    const resData = await response.json();
 
-    setAvailablePlaces(resData.places);
+    try {
+      const response = await fetch(import.meta.env.VITE_BASE_API + "/places", {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch places");
+      }
+
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    } catch (error) {
+      setError({
+        message:
+          error.message ||
+          "Could not fetch places, please check your connection and try again..",
+      });
+    }
+
     setIsFetching(false);
+  }
+
+  if (error) {
+    return <Error title="An Error Occurred!" message={error.message} />;
   }
 
   return (

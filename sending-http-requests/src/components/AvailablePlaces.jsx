531dce0e52d51407c7d4e9467e5fd7d4190f56fe
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-01 13:15:26 +0800

    feat(AvailablePlaces): use data from mockup server

--- sending-http-requests/src/components/AvailablePlaces.jsx
+++ sending-http-requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_BASE_API + "/places", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />

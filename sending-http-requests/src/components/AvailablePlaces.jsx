54f46bf10efc1782627626ada5fe3bc882a39e2e
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-01 14:18:58 +0800

    patch(AvailablePlaces): sort places from nearest place to the user location

--- sending-http-requests/src/components/AvailablePlaces.jsx
+++ sending-http-requests/src/components/AvailablePlaces.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 
+import { sortPlacesByDistance } from "../loc.js";
+
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
@@ -24,16 +26,25 @@ export default function AvailablePlaces({ onSelectPlace }) {
       }
 
       const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const sortedPlaces = sortPlacesByDistance(
+          resData.places,
+          position.coords.latitude,
+          position.coords.longitude
+        );
+
+        setAvailablePlaces(sortedPlaces);
+        setIsFetching(false);
+      });
     } catch (error) {
       setError({
         message:
           error.message ||
           "Could not fetch places, please check your connection and try again..",
       });
+      setIsFetching(false);
     }
-
-    setIsFetching(false);
   }
 
   if (error) {

83b40ca319d7a899fc1f9b924c8c0c1fdb13aa90
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-01 13:36:43 +0800

    feat(AvailablePlaces, Places): add loading state

--- sending-http-requests/src/components/AvailablePlaces.jsx
+++ sending-http-requests/src/components/AvailablePlaces.jsx
@@ -3,24 +3,29 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     fetchPlaces();
   }, []);
 
   async function fetchPlaces() {
+    setIsFetching(true);
     const response = await fetch(import.meta.env.VITE_BASE_API + "/places", {
       method: "GET",
     });
     const resData = await response.json();
 
     setAvailablePlaces(resData.places);
+    setIsFetching(false);
   }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data"
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />

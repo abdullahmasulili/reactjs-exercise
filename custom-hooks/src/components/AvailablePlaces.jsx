0dfb85e74cfc8f119fe9527d4675f699ed28734f
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-02 14:55:13 +0800

    refactor(AvailablePlaces): re-add sorting places by distance but using custom hook

--- custom-hooks/src/components/AvailablePlaces.jsx
+++ custom-hooks/src/components/AvailablePlaces.jsx
@@ -1,18 +1,31 @@
-import { useState, useEffect } from "react";
-
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
 import { useFetch } from "../hooks/useFetch.js";
 
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        coords.latitude,
+        coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
+
 export default function AvailablePlaces({ onSelectPlace }) {
   const {
     fetchedData: availablePlaces,
-    setFetchedData: setAvailablePlaces,
     error,
     isFetching,
-  } = useFetch(fetchAvailablePlaces, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;

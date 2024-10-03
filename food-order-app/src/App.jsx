d8f0ab45aa4dfc92f94732aa5573b6a147905207
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-03 19:29:08 +0800

    feat(App): add cart modal

--- food-order-app/src/App.jsx
+++ food-order-app/src/App.jsx
@@ -1,12 +1,25 @@
+import { useCallback, useRef } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
 import Products from "./components/Products";
 import ProductContextProvider from "./context/products-context";
 
 function App() {
+  const cartRef = useRef();
+
+  const handleOpenCart = useCallback(function handleOpenCart() {
+    cartRef.current.open();
+  }, []);
+
   return (
     <>
       <ProductContextProvider>
-        <Header />
+        <Modal ref={cartRef}>
+          <div className="cart">
+            <h2>Your Cart</h2>
+          </div>
+        </Modal>
+        <Header onCartClick={handleOpenCart} />
         <main>
           <Products
             loadingText="Retrieving Meals, Please wait..."
--- food-order-app/src/components/Header.jsx
+++ food-order-app/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import Logo from "../assets/logo.jpg";
 
-export default function Header() {
+export default function Header({ onCartClick }) {
   return (
     <header id="main-header">
       <h1 id="title">
         <img src={Logo} alt="application logo" />
         REACTFOOD
       </h1>
-      <button className="text-button">Cart (0)</button>
+      <button className="text-button" onClick={onCartClick}>
+        Cart (0)
+      </button>
     </header>
   );
 }

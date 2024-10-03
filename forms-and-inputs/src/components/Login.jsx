e7722d5c094bb0af79db2a260f96fd83e1665e20
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-03 12:07:10 +0800

    feat(Login): add validation

--- forms-and-inputs/src/components/Login.jsx
+++ forms-and-inputs/src/components/Login.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { useRef } from "react";
 
 export default function Login() {
+  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
   const emailRef = useRef();
   const passwordRef = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+
+    const isEmailValid = enteredEmail.includes("@");
+
+    if (!isEmailValid) {
+      setIsEmailInvalid(true);
+
+      return;
+    }
+
+    setIsEmailInvalid(false);
   }
 
   return (
@@ -16,6 +31,11 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={emailRef} />
+          {isEmailInvalid && (
+            <div className="control-error">
+              <p>Email is invalid. Please enter the valid email</p>
+            </div>
+          )}
         </div>
 
         <div className="control no-margin">

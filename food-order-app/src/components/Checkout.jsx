401a25e77c248dbe4e4908e2d02d71a7f47fd2d1
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-04 14:19:30 +0800

    feat(Checkout): add checkout form fields

--- food-order-app/src/components/Checkout.jsx
+++ food-order-app/src/components/Checkout.jsx
@@ -1,12 +1,19 @@
+import { currency } from "../util/currency";
+import Input from "./Input";
+
 export default function Checkout() {
   return (
     <>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-        voluptatibus distinctio nostrum iusto eaque? Tenetur vero aliquam quia
-        mollitia ea doloribus ipsa vitae quas a, iste quisquam optio illo.
-        Necessitatibus.
-      </p>
+      <p>Total Amount : {currency.format(84)}</p>
+      <form>
+        <Input type="text" label="Full Name" />
+        <Input type="email" label="E-mail Address" inputMode="email" />
+        <Input type="address" label="Street" />
+        <div className="control-row">
+          <Input type="text" label="Postal Code" />
+          <Input type="text" label="City" />
+        </div>
+      </form>
     </>
   );
 }

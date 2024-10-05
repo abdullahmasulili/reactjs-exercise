18f486023dc80b389dfe1407ba362198729e4cd4
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-05 20:48:47 +0800

    patch(Error): fix styling

--- food-order-app/src/components/Error.jsx
+++ food-order-app/src/components/Error.jsx
@@ -3,8 +3,8 @@ export default function Error({ message, title }) {
     <div
       className="error"
       style={{
-        margin: "auto",
-        padding: "1rem",
+        margin: "1rem auto",
+        padding: "0.5rem 1rem",
         backgroundColor: "#fca5a5",
         color: "#dc2626",
         borderRadius: "15px",

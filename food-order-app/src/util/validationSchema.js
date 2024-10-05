6a5c1ed854b3eb1588ba885e84bcde3a61d9dec0
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-05 17:46:54 +0800

    feat(util): add validationSchema utility file

---
+++ food-order-app/src/util/validationSchema.js
@@ -0,0 +1,14 @@
+import * as Yup from "yup";
+
+export const OrderSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(5, "Too Short!")
+    .max(50, "Too Long")
+    .required("Required"),
+  email: Yup.string().email("Invalid Email").required("Required"),
+  street: Yup.string().min(5, "Too Short").required("Required"),
+  ["postal-code"]: Yup.number("Invalid Input")
+    .min(3, "Too Short")
+    .required("Required"),
+  city: Yup.string().min(3, "Too Short").required("Required"),
+});

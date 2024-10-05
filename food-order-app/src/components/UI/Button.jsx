def9ecbef2a1da7503c8f300b25d2b70f8bd5472
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-05 12:13:56 +0800

    refactor(Button): add more props and handle default styling inside the component

--- food-order-app/src/components/UI/Button.jsx
+++ food-order-app/src/components/UI/Button.jsx
@@ -1,3 +1,10 @@
-export default function Button({ caption, ...props }) {
-  return <button {...props}>{caption}</button>;
+export default function Button({ children, className, textOnly, ...props }) {
+  const cssClasses = textOnly ? "text-button" : "button";
+  cssClasses.concat(` ${className}`);
+
+  return (
+    <button className={cssClasses} {...props}>
+      {children}
+    </button>
+  );
 }

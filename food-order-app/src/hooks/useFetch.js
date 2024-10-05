c4faf8ccb50b50ac6cd67b1b52ca6e2ca1041ab2
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-05 20:27:07 +0800

    patch(useFetch): add fetchMethod param for use effect checking

--- food-order-app/src/hooks/useFetch.js
+++ food-order-app/src/hooks/useFetch.js
@@ -1,26 +1,31 @@
 import { useCallback, useEffect, useState } from "react";
 
-export default function useFetch(fetchFn, initialValue) {
+export default function useFetch(fetchFn, initialValue, fetchMethod) {
   const [fetchedData, setFetchedData] = useState(initialValue);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
+  const fetchData = useCallback(
+    async function fetchData() {
+      setIsLoading(true);
 
-    try {
-      const data = await fetchFn();
-      setFetchedData(data);
-    } catch (error) {
-      setError({ message: error.message || "Failed to fetch data..." });
-    }
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data..." });
+      }
 
-    setIsLoading(false);
-  }, [fetchFn]);
+      setIsLoading(false);
+    },
+    [fetchFn]
+  );
 
   useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    if (fetchMethod === "GET") {
+      fetchData();
+    }
+  }, [fetchData, fetchMethod]);
 
-  return { fetchedData, isLoading, error, setFetchedData };
+  return { fetchedData, isLoading, error, fetchData };
 }

f27fc1496068daec051d248b8e3eff324d2a6058
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-05 17:05:13 +0800

    refactor(Cart): uplift the context handling

--- food-order-app/src/components/Cart.jsx
+++ food-order-app/src/components/Cart.jsx
@@ -3,12 +3,14 @@ import { useContext } from "react";
 import { CartContext } from "../context/products-cart-context";
 import { UserProgressContext } from "../context/user-progress-context";
 
+import { currency } from "../util/formatter.js";
+
 import CartItems from "./CartItems";
 import Button from "./UI/Button";
 import Modal from "./Modal";
 
 export default function Cart() {
-  const { products } = useContext(CartContext);
+  const { products, updateItemQuantity, cartTotal } = useContext(CartContext);
   const {
     hideModal: hideCart,
     showCheckout,
@@ -18,7 +20,21 @@ export default function Cart() {
   return (
     <Modal className="cart" open={progress === "cart"}>
       <h2>Your Cart</h2>
-      <CartItems />
+      <ul>
+        {products.length < 1 && (
+          <p>Nothing to show here. Add your desired meal first</p>
+        )}
+        {products.length < 1 &&
+          products.map((product) => (
+            <CartItems
+              key={product.id}
+              product={product}
+              onPlus={() => updateItemQuantity(product.id, 1)}
+              onMinus={() => updateItemQuantity(product.id, -1)}
+            />
+          ))}
+      </ul>
+      <p className="cart-total">{currency.format(cartTotal)}</p>
       <div className="modal-actions">
         <Button textOnly onClick={hideCart}>
           Close
--- food-order-app/src/components/CartItems.jsx
+++ food-order-app/src/components/CartItems.jsx
@@ -1,37 +1,20 @@
-import { useContext } from "react";
 import { currency } from "../util/formatter.js";
-import { CartContext } from "../context/products-cart-context.jsx";
+
 import Button from "./UI/Button.jsx";
 
-export default function CartItems() {
-  const { products, updateItemQuantity, cartTotal } = useContext(CartContext);
-
+export default function CartItems({ product, onPlus, onMinus }) {
   return (
-    <>
-      <ul>
-        {products.length < 1 && (
-          <p>Nothing to show here. Add your desired meal first</p>
-        )}
-        {products.length > 0 &&
-          products.map((product) => (
-            <li key={product.id} className="cart-item">
-              <p>
-                {`${product.name} - ${product.quantity} x 
-                ${currency.format(product.price)}`}
-              </p>
-              <div className="cart-item-actions">
-                <Button onClick={() => updateItemQuantity(product.id, -1)}>
-                  -
-                </Button>
-                <p>{product.quantity}</p>
-                <Button onClick={() => updateItemQuantity(product.id, 1)}>
-                  +
-                </Button>
-              </div>
-            </li>
-          ))}
-      </ul>
-      <p className="cart-total">{currency.format(cartTotal)}</p>
-    </>
+    <li className="cart-item">
+      <p>
+        {`${product.name} - ${product.quantity} x ${currency.format(
+          product.price
+        )}`}
+      </p>
+      <div className="cart-item-actions">
+        <Button onClick={onMinus}>-</Button>
+        <p>{product.quantity}</p>
+        <Button onClick={onPlus}>+</Button>
+      </div>
+    </li>
   );
 }

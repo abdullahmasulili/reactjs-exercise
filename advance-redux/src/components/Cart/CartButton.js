83ef4014154d7a149d956cfc19ba41adc7f6f5e4
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-08 16:49:45 +0800

    patch(CartButton): consume totalQuantity from state maangement

--- advance-redux/src/components/Cart/CartButton.js
+++ advance-redux/src/components/Cart/CartButton.js
@@ -5,11 +5,7 @@ import { uiActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
 const CartButton = () => {
-  const cartItems = useSelector((state) => state.cart.items);
-  const cartAmount = cartItems.reduce(
-    (totalItems, cartItem) => totalItems + cartItem.quantity,
-    0
-  );
+  const cartAmount = useSelector((state) => state.cart.totalQuantity);
   const dispatch = useDispatch();
   const { toggleCart } = uiActions;
 

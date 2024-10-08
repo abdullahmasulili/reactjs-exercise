bb24ba6bf707e424a819275aad3f1d6e688c62af
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-08 15:24:20 +0800

    feat(ProductItem): add handler for add to cart button

--- advance-redux/src/components/Shop/ProductItem.js
+++ advance-redux/src/components/Shop/ProductItem.js
@@ -1,8 +1,16 @@
+import { useDispatch } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cartSlice";
 
 const ProductItem = (props) => {
   const { title, price, description } = props.product;
+  const dispatch = useDispatch();
+  const { addItem } = cartActions;
+
+  function handleAddToCart() {
+    dispatch(addItem(props.product));
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +21,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={handleAddToCart}>Add to Cart</button>
         </div>
       </Card>
     </li>

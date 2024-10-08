14339e77c858c9b923e67e9479efaa1d4525fa2f
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-08 16:29:00 +0800

    feat(store): add ui state management

--- advance-redux/src/store/index.js
+++ advance-redux/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cartSlice";
+import cartReducer from "./cart-slice";
 import shopReducer from "./shopSlice";
+import uiReducer from "./ui-slice";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     shop: shopReducer,
+    ui: uiReducer,
   },
 });
 

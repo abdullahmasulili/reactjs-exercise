9ea5009915e769cdfa27acd0e71c1ba76ab5bbb3
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-08 14:30:36 +0800

    feat(CartButton): render total item inside the cart

--- advance-redux/src/components/Cart/CartButton.js
+++ advance-redux/src/components/Cart/CartButton.js
@@ -1,10 +1,17 @@
-import classes from './CartButton.module.css';
+import { useSelector } from "react-redux";
+import classes from "./CartButton.module.css";
 
-const CartButton = (props) => {
+const CartButton = ({ onClick }) => {
+  const cartItems = useSelector((state) => state.cart.items);
+  const cartAmount = cartItems.reduce(
+    (totalItems, cartItem) => totalItems + cartItem.quantity,
+    0
+  );
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={onClick}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartAmount}</span>
     </button>
   );
 };

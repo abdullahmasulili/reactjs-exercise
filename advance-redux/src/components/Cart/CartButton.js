02e51c5251f6a07a7a5d7b97210cb908a607bc0c
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-08 16:37:13 +0800

    patch(CartButton): move toggle cart action handler into CartButton from MainHeader component

--- advance-redux/src/components/Cart/CartButton.js
+++ advance-redux/src/components/Cart/CartButton.js
@@ -1,15 +1,24 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { uiActions } from "../../store/ui-slice";
+
 import classes from "./CartButton.module.css";
 
-const CartButton = ({ onClick }) => {
+const CartButton = () => {
   const cartItems = useSelector((state) => state.cart.items);
   const cartAmount = cartItems.reduce(
     (totalItems, cartItem) => totalItems + cartItem.quantity,
     0
   );
+  const dispatch = useDispatch();
+  const { toggleCart } = uiActions;
+
+  function handleToggleCart() {
+    dispatch(toggleCart());
+  }
 
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={classes.button} onClick={handleToggleCart}>
       <span>My Cart</span>
       <span className={classes.badge}>{cartAmount}</span>
     </button>
--- advance-redux/src/components/Layout/MainHeader.js
+++ advance-redux/src/components/Layout/MainHeader.js
@@ -1,23 +1,14 @@
-import { useDispatch } from "react-redux";
 import CartButton from "../Cart/CartButton";
 import classes from "./MainHeader.module.css";
-import { uiActions } from "../../store/ui-slice";
 
 const MainHeader = (props) => {
-  const dispatch = useDispatch();
-  const { toggleCart } = uiActions;
-
-  function handleToggleCart() {
-    dispatch(toggleCart());
-  }
-
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
       <nav>
         <ul>
           <li>
-            <CartButton onClick={handleToggleCart} />
+            <CartButton />
           </li>
         </ul>
       </nav>

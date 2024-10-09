a141adec73beeeb157ffa5a67e46ed080381a39c
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-09 16:53:30 +0800

    feat(Events): consumed fetched data from server

--- advance-routing/frontend/src/pages/Events.js
+++ advance-routing/frontend/src/pages/Events.js
@@ -1,3 +1,29 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import EventItem from "../components/EventItem";
+
+import { fetchEventsData } from "../store/events-actions.js";
+
 export default function EventsPage() {
-  return <div>EventsPage</div>;
+  const events = useSelector((state) => state.events.items);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEventsData());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Your Events List</h1>
+      <ul>
+        {events.length > 0 &&
+          events.map((event) => (
+            <li key={event.id}>
+              <EventItem event={event} />
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }

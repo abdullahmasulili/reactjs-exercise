b0ace64f0638f2e639c19360ee07189401ab5490
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-09 15:58:07 +0800

    feat(router): configured router and its initial components

--- advance-routing/frontend/src/router/index.js
+++ advance-routing/frontend/src/router/index.js
@@ -1,8 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
+import HomePage from "../pages/Home";
+import EventsLayout from "../layouts/EventsLayout";
+import EventsPage from "../pages/Events";
+import NewEventPage from "../pages/NewEvent";
+import EditEventPage from "../pages/EditEvent";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "events",
+        element: <EventsLayout />,
+        children: [
+          {
+            index: true,
+            elementL: <EventsPage />,
+          },
+          {
+            path: "new-event",
+            element: <NewEventPage />,
+          },
+          {
+            path: "edit-event",
+            element: <EditEventPage />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 

b82d6af6ab7767daee8c3cdce5d55d0802e1c2d4
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-09 13:43:28 +0800

    feat(router): use nested route to utilize layout component

--- multipage-spa/src/pages/Home.js
+++ multipage-spa/src/pages/Home.js
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        Go to <Link to="/products">products page</Link>
+      </p>
+    </>
+  );
 }
--- multipage-spa/src/router/index.js
+++ multipage-spa/src/router/index.js
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
+import RootLayout from "../layouts/RootLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+    ],
   },
 ]);
 

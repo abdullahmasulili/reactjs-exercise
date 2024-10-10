3aac27ac9b1a3614fcaf513a87bcd8a6ab051fac
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-10 17:44:08 +0800

    feat(EventDetail): render event item

--- advance-routing/frontend/src/pages/EventDetail.js
+++ advance-routing/frontend/src/pages/EventDetail.js
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
   const params = useParams();
   const id = params.eventId;
+  const events = useSelector((state) => state.events.items);
+  const event = events.find((item) => item.id === id);
 
-  return <h1>Event {id} Details</h1>;
+  return <EventItem event={event} />;
 }

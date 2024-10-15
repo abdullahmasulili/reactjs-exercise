a172d7004f17319de947ccfdc1c6abda65b0c588
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-15 14:44:41 +0800

    feat(Authentication): add authentication action handler

--- authentication/frontend/src/router/index.js
+++ authentication/frontend/src/router/index.js
@@ -15,7 +15,9 @@ import { action as manipulateEventAction } from "../components/EventForm";
 import NewsletterPage, {
   action as newsletterAction,
 } from "../pages/Newsletter";
-import AuthenticationPage from "../pages/Authentication";
+import AuthenticationPage, {
+  action as authAction,
+} from "../pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -65,6 +67,7 @@ const router = createBrowserRouter([
       {
         path: "auth",
         element: <AuthenticationPage />,
+        action: authAction,
       },
     ],
   },

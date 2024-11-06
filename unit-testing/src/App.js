1b2da1173e773c1f837d7077dcf809a38c6c549c
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-11-06 15:29:01 +0800

    feat(App): render posts component

--- unit-testing/src/App.js
+++ unit-testing/src/App.js
@@ -1,9 +1,11 @@
 import Greetings from "./components/Greetings";
 import "./App.css";
+import Posts from "./components/Posts";
 
 function App() {
   return (
     <div className="App">
+      <Posts />
       <Greetings />
     </div>
   );

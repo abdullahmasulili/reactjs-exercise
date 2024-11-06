f123d43341a91ea60ecfddef315afceda19aa4f9
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-11-06 15:15:57 +0800

    test(Greetings): add unit test for Greetings component

--- unit-testing/src/components/Greetings.jsx
+++ unit-testing/src/components/Greetings.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Greetings() {
+  const [changedText, setChangedText] = useState(false);
+
+  function handleChangeText() {
+    setChangedText(true);
+  }
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you</p>
+      {!changedText && <p>It's good to see you</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={handleChangeText}>Change Text</button>
     </div>
   );
 }
--- unit-testing/src/components/Greetings.test.jsx
+++ unit-testing/src/components/Greetings.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greetings from "./Greetings";
 
 describe("Greetings component", () => {
@@ -6,5 +7,37 @@ describe("Greetings component", () => {
     render(<Greetings />);
     const headingText = screen.getByText(/hello world/i);
     expect(headingText).toBeInTheDocument();
+  });
+
+  test("render initial text before button clicked", () => {
+    render(<Greetings />);
+
+    const initialText = screen.getByText(/good to see you/i);
+
+    expect(initialText).toBeInTheDocument();
+  });
+
+  test("render changed text after button clicked", () => {
+    render(<Greetings />);
+
+    const changeTextButton = screen.getByText("Change Text");
+
+    userEvent.click(changeTextButton);
+
+    const changedText = screen.getByText(/changed/i);
+
+    expect(changedText).toBeInTheDocument();
+  });
+
+  test("does not render initial text after button clicked", () => {
+    render(<Greetings />);
+
+    const changeTextButton = screen.getByText("Change Text");
+
+    userEvent.click(changeTextButton);
+
+    const initialText = screen.queryByText(/good to see you/i);
+
+    expect(initialText).toBeNull();
   });
 });

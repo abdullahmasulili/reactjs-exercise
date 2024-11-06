4194e6dcf50d8505b64e7d3142b3df77a4657e74
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-11-06 14:40:43 +0800

    feat(test): add test suite for Greetings component

--- unit-testing/src/components/Greetings.test.jsx
+++ unit-testing/src/components/Greetings.test.jsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Greetings";
 
-test("render Hello World", () => {
-  render(<Greetings />);
-  const headingText = screen.getByText(/hello world/i);
-  expect(headingText).toBeInTheDocument();
+describe("Greetings component", () => {
+  test("render Hello World", () => {
+    render(<Greetings />);
+    const headingText = screen.getByText(/hello world/i);
+    expect(headingText).toBeInTheDocument();
+  });
 });
